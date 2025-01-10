9bf0c89db36b704c03665b1cd2f8242541dc4c01
User <markpomidorchik@gamil.com>
2025-01-10 22:05:47 +0500

    construktors

--- app.js
+++ app.js
@@ -1,35 +1,115 @@
-//// arrayda berilgan qiymatga teng sonlarni topish
-const numbers = [1, 2, 3, 4, 5, 6];
-const bigsum = 7;
-function equalsum(arr, target) {
-    let couple = [];
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] + arr[j] === target) {
-                couple.push([arr[i], arr[j]]);
-            }
-        }
-    }
-    return couple;
-}
-console.log(equalsum(numbers, bigsum));
+// class Developer {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     coding() {
+//         console.log(`${this.name} is coding`);
+//     }
+// }
 
-//// palindrome sozni topish
-function isPalindrome(word) {
-    word = word.toLowerCase().replace(/[^a-z0-9]/g, '');
-    let reversed = word.split('').reverse().join('');
-    return word === reversed;
-}
-console.log(isPalindrome("anna"));
-console.log(isPalindrome("salom"));
-console.log(isPalindrome("A roza upala na lapu Azora"));
+// const developer1 = new Developer('Mark', 16);
+// developer1.coding();
+// console.log(developer1);
 
-//// anagramma sozni topish
+///// Animal
+// class Animal {
+//     constructor(name, type, sound) {
+//         this.name = name;
+//         this.type = type;
+//         this.sound = sound;
+//     }
+//     get makeSound() {
+//         console.log(`${this.name} makes ${this.sound} dude)`);
+//     }
+// }
+// class Dog extends Animal {
+//     super(name, type, sound) {
+//         this.name = name;
+//         this.type = type;
+//         this.sound = sound;
+//     }
+//     get makeSound() {
+//         console.log(`${this.name} makes ${this.sound} dude)`);
+//     }
+//     get bite() {
+//         console.log(`${this.name} tiwladi`);
+//     }
+// }
+// class Cat extends Animal {
+//     super(name, type, sound) {
+//         this.name = name;
+//         this.type = type;
+//         this.sound = sound;
+//     }
+//     get makeSound() {
+//         console.log(`${this.name} makes ${this.sound} dude)`);
+//     }
+//     get drink() {
+//         console.log(`${this.name} sut icvotti`);
+//     }
+// }
+// const dog = new Dog('bobik', 'it', 'woof');
+// console.log(dog);
+// const cat = new Cat('baroqvoy', 'mushu', 'meow');
+// console.log(cat);
 
-function anagramma(word1, word2) {
-    word1 = word1.toLowerCase().split('').sort().join('');
-    word2 = word2.toLowerCase().split('').sort().join('');
-    return word1 === word2;
-}
-console.log(anagramma("listen", "silent"));
-console.log(anagramma("olma", "banan")); 
+
+
+
+
+// ////students
+// class Student {
+//     constructor(name, age, grades,) {
+//         this.name = name;
+//         this.age = age;
+//         this.grades = [];
+
+//     }
+//     addGrade(grade) {
+//         this.grades.push(grade);
+//     }
+//     get average() {
+//         let sum = 0;
+//         for (let i = 0; i < this.grades.length; i++) {
+//             sum += this.grades[i];
+//         }
+//         return sum / this.grades.length;
+//     }
+
+// }
+// const student1 = new Student('Mark', 16);
+// student1.addGrade(5);
+// student1.addGrade(4);
+// student1.addGrade(3);
+// student1.addGrade(2);
+// console.log(student1);
+// console.log(student1.average + ' ortacha baho');
+
+///// car
+// class Car {
+//     constructor(brand, model, year, millage) {
+//         this.brand = brand;
+//         this.model = model;
+//         this.year = year;
+//         this.millage = millage;
+
+//     }
+//     get info() {
+//         console.log(`${this.brand} ${this.model} ${this.year} ${this.millage}`);
+//     }
+//     get age() {
+//         const currentYear = new Date().getFullYear();
+//         return currentYear - this.year;
+//     }
+//     drive(drive) {
+//         const km = this.millage + drive;
+//         this.millage = km;
+//     }
+
+// }
+// const car1 = new Car('Mersedes', 'Cls', 2023, 10000);
+// console.log(car1);
+// console.log(car1.age + ' yil');
+// car1.drive(1000);
+// console.log(car1.millage + ' km');
